8feaa6b9b524e34e104c45b420c5e853c438aa6f
Hamp <yongbeomkwak@gmail.com>
2023-06-04 20:52:47 +0900

    :lipstick: : 헤더

--- UikitPlayground/UikitPlayground/AppStoreClone/View/RecentRecordHeaderView.swift
+++ UikitPlayground/UikitPlayground/AppStoreClone/View/RecentRecordHeaderView.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class RecentRecordHeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBAction func removeAll(_ sender: Any) {
+        completionHandler?()
     }
-    */
+    //1. 넘겨주는 연결통로
+    var completionHandler: (() -> ())?
+    
+    override init(frame: CGRect) { //코드쪽에서 생성 시 호출
+        super.init(frame: frame)
+        self.setupView()
+    }
+    
+    required init?(coder aDecoder:NSCoder) { //StoryBoard에서 호출됨
+        super.init(coder: aDecoder)
+        self.setupView()
+    }
+    
+    private func setupView(){
+
+
+    }
 
 }
