24d7244d0e59be72f0edf31131fa145fa781573b
Hamp <yongbeomkwak@gmail.com>
2023-06-08 17:43:39 +0900

    :gear: :: 네트워크 매너지 만들기

--- UikitPlayground/UikitPlayground/Utility/Manager/NetworkManager.swift
+++ UikitPlayground/UikitPlayground/Utility/Manager/NetworkManager.swift
@@ -12,9 +12,32 @@ class NetworkManager {
     static let shared = NetworkManager()
     
     
-    
     func loadSearchResult(url:String,completion: @escaping (SearchResultModel) -> Void)
     {
+        guard let url = URL(string: url) else {return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            
+         //  guard let self else {return}
+            
+            guard  let data = data ,error == nil else {
+                DEBUG_LOG("ERROR Occured")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            guard let result = try? decoder.decode(SearchResultModel.self, from: data) else {return}
+            
+            DEBUG_LOG("RESULT:\(result)")
+            
+            completion(result)
+        }
+        
+        
+        DispatchQueue.global(qos: .background).async {
+            task.resume()
+        }
         
     }
     
