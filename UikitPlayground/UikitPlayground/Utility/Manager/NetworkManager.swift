32c7989e13976f05f7b897c425db48d90d496801
Hamp <yongbeomkwak@gmail.com>
2023-06-09 22:20:18 +0900

    :zap: :: 변경된 API를 이용하여 적용

--- UikitPlayground/UikitPlayground/Utility/Manager/NetworkManager.swift
+++ UikitPlayground/UikitPlayground/Utility/Manager/NetworkManager.swift
@@ -12,9 +12,7 @@ import Foundation
 class NetworkManager {
     
     static let shared = NetworkManager()
-    
-    private let searchApi = "https://itunes.apple.com/search"
-    
+        
     func parseData(requestURL:URL,completion:@escaping (Data) -> Void){
         
         let task = URLSession.shared.dataTask(with: requestURL) { data, response, error in
@@ -39,26 +37,8 @@ class NetworkManager {
     
     func loadSearchResult(term:String,completion:@escaping (SearchResultModel) -> Void)
     {
-        guard var urlComponent = URLComponents(string: searchApi) else {return }
-        
-        var queryItemArray:[URLQueryItem] = []
-        queryItemArray.append( URLQueryItem(name: "term", value: term ))
-        queryItemArray.append(URLQueryItem(name: "country", value: "kr"))
-        queryItemArray.append(URLQueryItem(name: "entity", value: "software"))
-        queryItemArray.append(URLQueryItem(name: "limit", value: "10"))
-        
-        urlComponent.queryItems = queryItemArray
-        //URL에 Query 추가
-        
-        
-        
-        // request method 는 기본적으로 GET 이다. 혹시나 POST 등을 사용하고 싶다면 URLRequest 을 만들어야한다.
-        // var request = URLRequest(url: requestURL)
-        // request.httpMethod = "GET"
-        
-        guard let requestURL = urlComponent.url else { return }
-        
-        parseData(requestURL: requestURL) { data in
+  
+        parseData(requestURL: SearchAPI.fetchSearch(text: term).url) { data in
             let decoder = JSONDecoder()
             guard let result = try? decoder.decode(SearchResultModel.self, from: data) else {return}
             
