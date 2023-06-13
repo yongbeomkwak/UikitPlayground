bc5b383a3a40d89ba19d056f2981f7b521e8ce7c
Hamp <yongbeomkwak@gmail.com>
2023-06-13 18:47:38 +0900

    :sparkles: :: 컴바인 스켈레톤

---
+++ UikitPlayground/UikitPlayground/CombineClone/Resource/CombineCloneViewController.swift
@@ -0,0 +1,56 @@
+//
+//  RxCloneViewController.swift
+//  UikitPlayground
+//
+//  Created by yongbeomkwak on 2023/06/13.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class CombineCloneViewController: UIViewController,ViewControllerFromStoryBoard {
+
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    private var viewModel : CombineCloneViewModel!
+    private lazy var input = CombineCloneViewModel.Input()
+    private lazy var output = viewModel.transform(from:input)
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bindRx()
+    }
+    
+    public static func viewController() -> CombineCloneViewController {
+        
+        let vc = CombineCloneViewController.viewController(storyBoardName: "CombineClone", bundle: .main)
+        
+        vc.viewModel = CombineCloneViewModel()
+        
+        
+        
+        return vc
+    }
+    
+
+
+
+}
+
+extension CombineCloneViewController {
+    
+    private func bindRx(){
+        
+
+        
+    }
+    
+}
