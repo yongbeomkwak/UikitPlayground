a5feaffe2a2317c34d93bd5d6754baa08c406927
Hamp <yongbeomkwak@gmail.com>
2023-06-20 15:03:35 +0900

    :lipstick: :: 셀 생성

---
+++ UikitPlayground/UikitPlayground/GitClone/Sources/View/GitRepoTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  GitRepoTableViewCell.swift
+//  UikitPlayground
+//
+//  Created by yongbeomkwak on 2023/06/20.
+//
+
+import UIKit
+
+class GitRepoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var repoTitleLabel: UILabel!
+    @IBOutlet weak var starImageView: UIImageView!
+    @IBOutlet weak var numStarLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        self.starImageView.image = UIImage(systemName: "star.fill")
+        self.starImageView.tintColor = .yellow
+        
+        self.repoTitleLabel.font = .systemFont(ofSize: 20, weight: .black)
+        
+        self.numStarLabel.font = .systemFont(ofSize: 10, weight: .regular)
+        self.numStarLabel.tintColor = .systemGray4
+        
+    }
+
+}
+
+extension GitRepoTableViewCell {
+    
+    
+    
+}
